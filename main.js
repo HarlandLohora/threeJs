20b5d47dbd9da1273a6b5be1c1e3c0cdfa395a9a
Harland Lohora <lohoraharland@gmail.com>
2019-05-05 00:22:40 -0500

    Agregando Malla

--- main.js
+++ main.js
@@ -6,6 +6,9 @@
   //Primero rango de vista, radio de  la imagen
   const aspectRadio = window.innerWidth / window.innerHeight;
   let camera = new THREE.PerspectiveCamera(75,aspectRadio,0.1,100);
+  camera.position.z = 5;
+  camera.position.y = 2;
+
   //Definiendo render
   let renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth,window.innerHeight);
@@ -13,5 +16,29 @@
   //Creando el renderer dentro de body
   document.body.appendChild( renderer.domElement );
 
-  renderer.render( scene,camera );
+  //Creando mesh - malla, requiere una geometria y un material.
+  let geometry       = new THREE.BoxGeometry(1,1,1);
+  let groundMaterial = new THREE.MeshPhongMaterial({
+    color: 0xffffff
+  });
+
+  let mesh = new THREE.Mesh( geometry, groundMaterial );
+
+  //Agregando elemento/malla a la escena
+  scene.add( mesh );
+
+  //Agregando Luces
+  let pointLight = new THREE.PointLight(0xdfebff);
+  pointLight.position.y = 30;
+  scene.add( new THREE.AmbientLight( 0x404040 ));
+  scene.add( pointLight );
+
+  //requestAnimationFrame calcula la cantidad de frames que se requieren
+  function loop(){
+    requestAnimationFrame( loop );
+    renderer.render( scene,camera );
+  }
+
+  loop();
+
 })();
